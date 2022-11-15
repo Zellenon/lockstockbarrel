23c76a269cffc16f96ce10442446efd5c250c280
Boofsnorf <boofsnorf@pop-os.localdomain>
2022-11-15 02:39:19 -0600

    removed normalize, can cause errors

--- src/utils.rs
+++ src/utils.rs
@@ -10,7 +10,7 @@ pub fn screen_to_world(p: Vec2, camera_transform: &Transform, windows: &Windows)
 }
 
 pub fn get_angle(direction: Vec3) -> Quat {
-    let direction = Vec3::normalize(direction);
-    let right = Vec3::new(0., 0., 1.).cross(direction).normalize();
+    let direction = Vec3::normalize_or_zero(direction);
+    let right = Vec3::new(0., 0., 1.).cross(direction).normalize_or_zero();
     Quat::from_mat3(&Mat3::from_cols(right, direction, Vec3::new(0., 0., -1.)))
 }
