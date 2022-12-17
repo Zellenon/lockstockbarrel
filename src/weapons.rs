1a74610c29f6f67ae3ed411f8b0a0a4c8ddd84c3
Zellenon <Zellenon@gmail.com>
2022-12-17 10:35:43 -0600

    added cooldowns, weapons plugin

--- src/weapons.rs
+++ src/weapons.rs
@@ -7,9 +7,29 @@ use crate::{
     projectile::{Damaging, Knockback, Lifespan, ProjectileBundle},
 };
 
+pub struct WeaponPlugin;
+
+impl Plugin for WeaponPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<FireWeaponEvent>()
+            .add_system(fire_weapons)
+            .add_system(tick_cooldowns)
+            .add_system(reset_weapon_cooldowns)
+            .add_system(enable_weapons_on_cooldown);
+    }
+}
+
 #[derive(Component)]
 pub struct Weapon {
-    fire_func: Box<dyn Fn(&mut WeaponArguments) + Send + Sync>,
+    pub can_fire: bool,
+    pub fire_mode: WeaponFireMode,
+    pub fire_func: Box<dyn Fn(&mut WeaponArguments) + Send + Sync>,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum WeaponFireMode {
+    SemiAuto,
+    FullAuto,
 }
 
 pub struct WeaponArguments<'c, 'w, 's, 'c2, 'w2, 's2> {
@@ -20,24 +40,31 @@ pub struct WeaponArguments<'c, 'w, 's, 'c2, 'w2, 's2> {
     pub transforms: Query<'c2, 'w2, &'s2 Transform>,
 }
 
+#[derive(Component)]
+pub struct Cooldown {
+    pub max: f32,
+    pub timer: Timer,
+}
+
+impl Cooldown {
+    pub fn new(max: f32) -> Self {
+        Self {
+            max,
+            timer: Timer::from_seconds(max, TimerMode::Once),
+        }
+    }
+}
+
 pub struct FireWeaponEvent {
     pub weapon: Entity,
     pub target: Option<Entity>,
-}
-
-pub struct WeaponPlugin;
-
-impl Plugin for WeaponPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_event::<FireWeaponEvent>().add_system(fire_weapons);
-    }
 }
 
 pub fn fire_weapons(
     mut events: EventReader<FireWeaponEvent>,
     mut commands: Commands,
     cursor: Query<Entity, With<CursorTracker>>,
-    weapons: Query<(&Weapon, &Parent), With<Weapon>>,
+    weapons: Query<(&Weapon, &Parent)>,
     transforms: Query<&Transform>,
 ) {
     let mut args = WeaponArguments {
@@ -55,39 +82,74 @@ pub fn fire_weapons(
     // args.target = None;
 }
 
-pub fn make_peashooter() -> Weapon {
-    Weapon {
-        fire_func: Box::new(move |a: &mut WeaponArguments| {
-            let parent_transform = a.transforms.get(a.parent).unwrap().clone();
-            let cursor_transform = a.transforms.get(a.cursor).unwrap().clone();
-            let fire_direction =
-                Vec3::normalize(cursor_transform.translation - parent_transform.translation);
-            a.commands
-                .spawn((
-                    ProjectileBundle {
-                        velocity: Velocity {
-                            linvel: fire_direction.truncate() * 7000.,
-                            angvel: 0.,
-                        },
-                        ..Default::default()
-                    },
-                    Knockback(250.),
-                    Damaging(20.),
-                    Ccd::enabled(),
-                    Lifespan::default(),
-                ))
-                .insert(GeometryBuilder::build_as(
-                    &shapes::Circle {
-                        radius: 5.,
-                        center: Vec2::ZERO,
-                    },
-                    DrawMode::Outlined {
-                        fill_mode: FillMode::color(Color::WHITE),
-                        outline_mode: StrokeMode::color(Color::BLACK),
-                    },
-                    parent_transform
-                        .with_translation(parent_transform.translation + fire_direction * 30.),
-                ));
-        }),
+fn reset_weapon_cooldowns(
+    mut events: EventReader<FireWeaponEvent>,
+    mut weapon_query: Query<(&mut Weapon, &mut Cooldown)>,
+) {
+    for FireWeaponEvent {
+        weapon: weapon_entity,
+        target: _,
+    } in events.iter()
+    {
+        let (mut weapon, mut cooldown) = weapon_query.get_mut(*weapon_entity).unwrap();
+        weapon.can_fire = false;
+        cooldown.timer = Timer::from_seconds(cooldown.max, TimerMode::Once);
     }
+}
+
+fn enable_weapons_on_cooldown(mut weapon_query: Query<(&mut Weapon, &mut Cooldown)>) {
+    for (mut weapon, _) in weapon_query
+        .iter_mut()
+        .filter(|(w, c)| (!w.can_fire) && c.timer.finished())
+    {
+        weapon.can_fire = true;
+    }
+}
+
+fn tick_cooldowns(mut cooldown_query: Query<&mut Cooldown>, time: Res<Time>) {
+    for mut cooldown in cooldown_query.iter_mut() {
+        cooldown.timer.tick(time.delta());
+    }
+}
+
+pub fn make_peashooter() -> (Weapon, Cooldown) {
+    (
+        Weapon {
+            can_fire: true,
+            fire_mode: WeaponFireMode::FullAuto,
+            fire_func: Box::new(move |a: &mut WeaponArguments| {
+                let parent_transform = a.transforms.get(a.parent).unwrap().clone();
+                let cursor_transform = a.transforms.get(a.cursor).unwrap().clone();
+                let fire_direction =
+                    Vec3::normalize(cursor_transform.translation - parent_transform.translation);
+                a.commands
+                    .spawn((
+                        ProjectileBundle {
+                            velocity: Velocity {
+                                linvel: fire_direction.truncate() * 7000.,
+                                angvel: 0.,
+                            },
+                            ..Default::default()
+                        },
+                        Knockback(250.),
+                        Damaging(20.),
+                        Ccd::enabled(),
+                        Lifespan::default(),
+                    ))
+                    .insert(GeometryBuilder::build_as(
+                        &shapes::Circle {
+                            radius: 5.,
+                            center: Vec2::ZERO,
+                        },
+                        DrawMode::Outlined {
+                            fill_mode: FillMode::color(Color::WHITE),
+                            outline_mode: StrokeMode::color(Color::BLACK),
+                        },
+                        parent_transform
+                            .with_translation(parent_transform.translation + fire_direction * 30.),
+                    ));
+            }),
+        },
+        Cooldown::new(0.3),
+    )
 }
