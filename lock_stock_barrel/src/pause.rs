5d29895ad61c6f9c0abff629f008aae8a3ac41b5
Zellenon <Zellenon@gmail.com>
2023-01-10 01:30:21 -0600

    added pause menu

--- lock_stock_barrel/src/pause.rs
+++ lock_stock_barrel/src/pause.rs
@@ -1,9 +1,32 @@
-use bevy::prelude::*;
+use bevy::prelude::{Commands, Input, KeyCode, Res, ResMut};
+use bevy_egui::{egui, EguiContext};
+use iyes_loopless::state::NextState;
 
-pub struct PausePlugin;
+use crate::states::{AppState, InGameMenu};
 
-impl Plugin for PausePlugin {
-    fn build(&self, app: &mut App) {
-        // app.add_system(system)
+pub(crate) fn pause_on_esc(mut commands: Commands, input: Res<Input<KeyCode>>) {
+    if input.pressed(KeyCode::Escape) {
+        commands.insert_resource(NextState(InGameMenu::Pause))
     }
+}
+
+pub(crate) fn pause_gui(mut commands: Commands, mut root: ResMut<EguiContext>) {
+    egui::Window::new("Pause")
+        .resizable(false)
+        .collapsible(false)
+        .scroll2([false, false])
+        .enabled(true)
+        .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::default())
+        .show(root.ctx_mut(), |ui| {
+            if ui.button("Resume").clicked() {
+                commands.insert_resource(NextState(InGameMenu::None));
+            }
+            if ui.button("Options").clicked() {
+                commands.insert_resource(NextState(InGameMenu::Options));
+            }
+            if ui.button("Quit").clicked() {
+                commands.insert_resource(NextState(AppState::MainMenu));
+                commands.insert_resource(NextState(InGameMenu::None));
+            }
+        });
 }
--- lock_stock_barrel/src/states.rs
+++ lock_stock_barrel/src/states.rs
@@ -1,10 +1,17 @@
 use bevy::{
     app::AppExit,
-    prelude::{Commands, EventWriter, Input, KeyCode, Plugin, Res, SystemSet},
+    prelude::{
+        Commands, DespawnRecursiveExt, Entity, EventWriter, Input, KeyCode, Plugin, Query, Res,
+        SystemSet, With,
+    },
 };
 use iyes_loopless::prelude::*;
+use twin_stick::{actors::Actor, bevy_rapier2d::prelude::RigidBody};
 
-use crate::mainmenu::main_menu_gui;
+use crate::{
+    mainmenu::main_menu_gui,
+    pause::{pause_gui, pause_on_esc},
+};
 
 pub struct StatePlugin;
 
@@ -12,17 +19,26 @@ impl Plugin for StatePlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_loopless_state(AppState::MainMenu);
         app.add_loopless_state(GameState::PlayingArena);
-        app.add_system(exit.run_in_state(AppState::Exit));
-        // app.add_system(test_system);
-        app.add_system(main_menu_gui.run_in_state(AppState::MainMenu));
+        app.add_loopless_state(InGameMenu::None);
 
-        // app.add_system(main_menu_gui);
+        app.add_system(exit.run_in_state(AppState::Exit));
+        app.add_enter_system(AppState::MainMenu, unload_world);
+        app.add_system(main_menu_gui.run_in_state(AppState::MainMenu));
+        app.add_system(
+            pause_gui
+                .run_in_state(AppState::Game)
+                .run_in_state(InGameMenu::Pause),
+        );
+
+        app.add_system(pause_on_esc.run_in_state(AppState::Game));
     }
 }
 
-fn test_system(mut commands: Commands, input: Res<Input<KeyCode>>) {
-    if input.pressed(KeyCode::Space) {
-        commands.insert_resource(NextState(AppState::Exit));
+fn unload_world(mut commands: Commands, gameworld_entities: Query<Entity, With<RigidBody>>) {
+    for actor in gameworld_entities.into_iter() {
+        let mut entity = commands.entity(actor);
+        entity.despawn_descendants();
+        entity.despawn();
     }
 }
 
@@ -30,21 +46,21 @@ fn test_system(mut commands: Commands, input: Res<Input<KeyCode>>) {
 pub enum AppState {
     MainMenu,
     Game,
-    Pause,
     Exit,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum GameState {
     PlayingArena,
-    InMenu(InGameMenu),
     Map,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum InGameMenu {
+    None,
     Inventory,
     Pause,
+    Options,
 }
 
 fn exit(mut app_exit_events: EventWriter<AppExit>) {
