01804df8ceda7eebb1f478af1db29981ac46093e
Zellenon <Zellenon@gmail.com>
2023-02-20 17:40:28 -0600

    added data-based level-spawning

--- lock_stock_barrel/src/game/level.rs
+++ lock_stock_barrel/src/game/level.rs
@@ -1,16 +1,52 @@
 use bevy::{ecs::system::EntityCommands, prelude::*};
-use bevy_composable::{ComponentTree, EntityCommandSet};
+use bevy_composable::{spawn_complex, ComponentTree, EntityCommandSet};
 use bevy_prototype_lyon::prelude as lyon;
-use bevy_prototype_lyon::prelude::ShapePlugin;
 use std::sync::Arc;
 use twin_stick::bevy_rapier2d::prelude::{Collider, RigidBody};
 
+type LevelMap = Vec<Vec<bool>>;
+
 #[derive(Resource)]
 pub struct Level {
-    pub arena_map: Vec<Vec<u8>>,
+    pub arena_map: LevelMap,
+    pub resolution: f32,
 }
 
-fn spawn_arena_from_map(mut commands: Commands, map: Vec<Vec<u8>>) {}
+pub fn to_map(map: Vec<Vec<u8>>) -> LevelMap {
+    map.iter()
+        .map(|w| w.iter().map(|x| *x == 1).collect())
+        .collect()
+}
+
+pub fn spawn_arena_from_map(mut commands: Commands, level: &Level) {
+    let y_len = (level.arena_map.iter().count() as f32) * level.resolution;
+    let x_len = (level
+        .arena_map
+        .iter()
+        .map(|w| w.iter().count())
+        .max()
+        .unwrap() as f32)
+        * level.resolution;
+    let mut i = 0.;
+    for row in level.arena_map.iter() {
+        let mut j = 0.;
+        for block in row.iter() {
+            if *block {
+                spawn_complex(
+                    &mut commands,
+                    wall(
+                        j * level.resolution - (x_len * 0.5),
+                        (y_len * 0.5) - i * level.resolution,
+                        level.resolution,
+                        level.resolution,
+                    ),
+                )
+            };
+            j = j + 1.;
+        }
+        i = i + 1.;
+    }
+}
 
 pub fn wall(x: f32, y: f32, width: f32, height: f32) -> ComponentTree {
     (Arc::new(move |e: &mut EntityCommands| {
@@ -24,11 +60,10 @@ pub fn wall(x: f32, y: f32, width: f32, height: f32) -> ComponentTree {
                     fill_mode: lyon::FillMode::color(Color::TEAL),
                     outline_mode: lyon::StrokeMode::color(Color::TEAL),
                 },
-                Transform::default(),
+                Transform::from_xyz(x, y, 0.0),
             ),
             RigidBody::Fixed,
             Collider::cuboid(width / 2., height / 2.),
-            Transform::from_xyz(x, y, 0.0),
         ));
     }) as EntityCommandSet)
         .into()
