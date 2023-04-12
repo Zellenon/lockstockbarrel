302ef49b6593ac0608a4f04cac0b9691408f4c5b
Zellenon <Zellenon@gmail.com>
2023-04-12 14:59:18 -0500

    moved weapons and actor bits to their own folder

---
+++ lock_stock_barrel/src/content/actor_bits.rs
@@ -0,0 +1,51 @@
+use std::sync::Arc;
+
+use bevy::{
+    ecs::system::EntityCommands,
+    prelude::{Handle, Image, Vec2},
+    sprite::Sprite,
+};
+use bevy_composable::{ComponentTree, EntityCommandSet};
+use twin_stick::{
+    actors::{Legs, Tracking},
+    bevy_mod_transform2d::transform2d::Transform2d,
+    transform2d_mods::Sprite2dBundle,
+};
+
+pub fn basic_head(head_tex: Handle<Image>) -> ComponentTree {
+    let tex = head_tex.clone();
+    let func = move |parent: &mut EntityCommands| {
+        parent.insert((
+            Sprite2dBundle {
+                sprite: Sprite {
+                    custom_size: Vec2::new(40., 40.).into(),
+                    ..Default::default()
+                },
+                texture: tex.clone(),
+                ..Default::default()
+            },
+            Tracking(None),
+        ));
+    };
+    (Arc::new(func) as EntityCommandSet).into()
+}
+
+pub fn basic_legs(leg_tex: Handle<Image>) -> ComponentTree {
+    let tex = leg_tex.clone();
+    let func = move |parent: &mut EntityCommands| {
+        parent.insert((
+            Sprite2dBundle {
+                sprite: Sprite {
+                    custom_size: Vec2::new(30., 35.).into(),
+                    ..Default::default()
+                },
+                transform: Transform2d::from_xy(0., 0.).with_z_translation(-1.),
+                texture: tex.clone(),
+                ..Default::default()
+            },
+            Tracking(None),
+            Legs::default(),
+        ));
+    };
+    (Arc::new(func) as EntityCommandSet).into()
+}
---
+++ lock_stock_barrel/src/content/weapons.rs
@@ -0,0 +1,100 @@
+use std::{sync::Arc, time::Duration};
+
+use bevy::{
+    ecs::system::EntityCommands,
+    prelude::{Color, Handle, Name, Vec2},
+    sprite::{Material2d, Mesh2dHandle},
+};
+use bevy_composable::{spawn_complex, ComponentTree, EntityCommandSet};
+use bevy_mod_transform2d::transform2d::Transform2d;
+use bevy_prototype_lyon::{
+    prelude::{Fill, GeometryBuilder, ShapeBundle, Stroke},
+    render::ShapeMaterial,
+    shapes,
+};
+use twin_stick::{
+    bevy_rapier2d::prelude::{Ccd, Velocity},
+    projectile::{
+        Damaging, Knockback, Lifespan, Projectile, ProjectileBundle, ProjectileHitBehavior,
+        ProjectileImpactBehavior,
+    },
+    weapons::{Cooldown, Weapon, WeaponArguments, WeaponFireMode},
+};
+
+use crate::game::level::wall;
+
+pub fn peashooter() -> ComponentTree {
+    let func = move |parent: &mut EntityCommands| {
+        parent.insert((
+            Weapon {
+                can_fire: true,
+                fire_mode: WeaponFireMode::FullAuto,
+                fire_func: Box::new(move |a: &mut WeaponArguments| {
+                    let parent_transform = a.transforms.get(a.parent).unwrap().clone();
+                    let cursor_transform = a.transforms.get(a.cursor).unwrap().clone();
+                    let fire_direction = Vec2::normalize(
+                        cursor_transform.translation - parent_transform.translation,
+                    );
+                    a.commands.spawn((
+                        ProjectileBundle {
+                            velocity: Velocity {
+                                linvel: fire_direction * 800.,
+                                angvel: 0.,
+                            },
+                            transform: Transform2d {
+                                translation: parent_transform.translation + fire_direction * 100.,
+                                ..Default::default()
+                            },
+                            ..Default::default()
+                        },
+                        GeometryBuilder::build_as(&shapes::Circle {
+                            radius: 5.,
+                            center: Vec2::ZERO,
+                        }),
+                        Fill::color(Color::YELLOW),
+                        Stroke::new(Color::BLACK, 2.0),
+                        Knockback(250.),
+                        Damaging(20.),
+                        Ccd::enabled(),
+                        // Lifespan::default(),
+                        Mesh2dHandle::default(),
+                        Handle::<ShapeMaterial>::default(),
+                        Name::new("Bullet"),
+                    ));
+                }),
+            },
+            Cooldown::new(0.3),
+        ));
+    };
+    (Arc::new(func) as EntityCommandSet).into()
+}
+
+pub fn wallgun() -> ComponentTree {
+    let func = move |parent: &mut EntityCommands| {
+        parent.insert((
+            Cooldown::new(0.3),
+            Weapon {
+                can_fire: true,
+                fire_mode: WeaponFireMode::SemiAuto,
+                fire_func: Box::new(move |a: &mut WeaponArguments| {
+                    let parent_transform = a.transforms.get(a.parent).unwrap().clone();
+                    let cursor_transform = a.transforms.get(a.cursor).unwrap().clone();
+                    let fire_direction = Vec2::normalize(
+                        cursor_transform.translation - parent_transform.translation,
+                    );
+
+                    spawn_complex(
+                        a.commands,
+                        wall(
+                            parent_transform.translation.x,
+                            parent_transform.translation.y,
+                            50.,
+                            50.,
+                        ),
+                    );
+                }),
+            },
+        ));
+    };
+    (Arc::new(func) as EntityCommandSet).into()
+}
