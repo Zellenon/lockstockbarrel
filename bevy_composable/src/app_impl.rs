49eab4d2b9f151391cf812070cc33a454bc8d251
Zellenon <Zellenon@gmail.com>
2023-04-14 14:27:43 -0500

    split composable files

---
+++ bevy_composable/src/app_impl.rs
@@ -0,0 +1,31 @@
+use std::ops;
+use std::sync::Arc;
+
+use bevy::ecs::system::EntityCommands;
+use bevy::prelude::*;
+
+use crate::tree::ComponentTree;
+
+pub trait ComplexSpawnable {
+    fn spawn_complex(&mut self, tree: ComponentTree) -> Entity;
+}
+
+impl ComplexSpawnable for Commands<'_, '_> {
+    fn spawn_complex(&mut self, tree: ComponentTree) -> Entity {
+        let mut entity = &mut self.spawn_empty();
+        spawn_complex_inner(entity, &tree);
+        entity.id()
+    }
+}
+
+fn spawn_complex_inner(entity: &mut EntityCommands, component_tree: &ComponentTree) {
+    for command in component_tree.commands.iter() {
+        command(entity);
+    }
+    for child in component_tree.children.iter() {
+        entity.with_children(|w| {
+            let mut child_entity = w.spawn_empty();
+            spawn_complex_inner(&mut child_entity, child);
+        });
+    }
+}
---
+++ bevy_composable/src/tree.rs
@@ -0,0 +1,64 @@
+use std::ops;
+use std::sync::Arc;
+
+use bevy::ecs::system::EntityCommands;
+use bevy::prelude::*;
+
+pub type EntityCommandSet = Arc<dyn Fn(&mut EntityCommands) + Send + Sync>;
+
+#[derive(Clone)]
+pub struct ComponentTree {
+    pub commands: Vec<EntityCommandSet>,
+    pub children: Vec<ComponentTree>,
+}
+
+impl From<EntityCommandSet> for ComponentTree {
+    fn from(value: EntityCommandSet) -> Self {
+        ComponentTree {
+            commands: vec![value],
+            children: Vec::new(),
+        }
+    }
+}
+
+impl ops::Add<ComponentTree> for ComponentTree {
+    type Output = ComponentTree;
+
+    fn add(self, rhs: ComponentTree) -> Self::Output {
+        Self {
+            commands: [self.commands.as_slice(), rhs.commands.as_slice()].concat(),
+            children: [self.children.as_slice(), rhs.children.as_slice()].concat(),
+        }
+    }
+}
+
+impl ops::Add<EntityCommandSet> for ComponentTree {
+    type Output = ComponentTree;
+
+    fn add(self, rhs: EntityCommandSet) -> Self::Output {
+        Self {
+            commands: [self.commands.as_slice(), vec![rhs].as_slice()].concat(),
+            children: self.children,
+        }
+    }
+}
+
+impl ops::Shl<ComponentTree> for ComponentTree {
+    type Output = ComponentTree;
+
+    fn shl(self, rhs: ComponentTree) -> Self::Output {
+        Self {
+            commands: self.commands,
+            children: [self.children.as_slice(), vec![rhs].as_slice()].concat(),
+        }
+    }
+}
+
+impl Default for ComponentTree {
+    fn default() -> Self {
+        ComponentTree {
+            commands: Vec::default(),
+            children: Vec::default(),
+        }
+    }
+}
