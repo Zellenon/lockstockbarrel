c315a0537e5ccdbff27ece43df96c92c9c37075d
Zellenon <Zellenon@gmail.com>
2023-04-18 11:50:50 -0500

    added turborand

--- twin_stick/src/lib.rs
+++ twin_stick/src/lib.rs
@@ -1,10 +1,12 @@
 pub extern crate bevy_mod_transform2d;
 pub extern crate bevy_rapier2d;
+pub extern crate bevy_turborand;
 use bevy::prelude::{App, Plugin, ResMut, Vec2};
 use bevy_mod_transform2d::{transform2d::Transform2d, Transform2dPlugin};
 
 use bevy_prototype_lyon::prelude::ShapePlugin;
 use bevy_rapier2d::prelude::{NoUserData, RapierConfiguration, RapierPhysicsPlugin};
+use bevy_turborand::RngPlugin;
 
 use self::{
     actors::ActorPlugin, ai::AIPlugin, player::PlayerPlugin, projectile::ProjectilePlugin,
@@ -29,6 +31,7 @@ impl Plugin for TwinStickPlugin {
             .add_plugin(ShapePlugin);
 
         app.add_plugin(PlayerPlugin)
+            .add_plugin(RngPlugin::default())
             .add_plugin(ActorPlugin)
             .add_plugin(WeaponPlugin)
             .add_plugin(AIPlugin)
