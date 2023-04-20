388ed9f5deab699cc3a5d74ddbea0cf1823a29a0
Zellenon <Zellenon@gmail.com>
2023-04-19 21:51:11 -0500

    added a plugin for content checks

--- lock_stock_barrel/src/content/mod.rs
+++ lock_stock_barrel/src/content/mod.rs
@@ -2,15 +2,29 @@ use std::sync::Arc;
 
 use bevy::{
     ecs::system::EntityCommands,
-    prelude::{Entity, Vec2},
+    prelude::{Entity, Plugin, Vec2},
 };
 use bevy_composable::tree::EntityCommandSet;
 use bevy_mod_transform2d::transform2d::Transform2d;
 use twin_stick::actors::Tracking;
 
+use self::stats::{ensure_health, ensure_speed, sync_health_to_health, sync_speed_to_speed};
+
 pub mod actor_bits;
 pub mod enemies;
+pub mod stats;
 pub mod weapons;
+
+pub struct ContentPlugin;
+
+impl Plugin for ContentPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_system(ensure_speed);
+        app.add_system(ensure_health);
+        app.add_system(sync_speed_to_speed);
+        app.add_system(sync_health_to_health);
+    }
+}
 
 pub fn shift_pos(pos: impl Into<Vec2>) -> EntityCommandSet {
     let new_pos = pos.into();
