db96c083dcdbf85e7892dd11897662ce0373e937
Zellenon <zellenon@gmail.com>
2024-01-07 22:23:19 -0600

    changed projectile graphics to new vector library

--- src/content/projectile.rs
+++ src/content/projectile.rs
@@ -1,30 +1,21 @@
 use bevy::{
-    prelude::{Color, Handle, Name, Vec2},
+    prelude::{Color, Name},
     sprite::Mesh2dHandle,
 };
 use bevy_composable::{
     tree::{ComponentTree, EntityCommandSet},
     CT,
 };
-use bevy_prototype_lyon::{
-    prelude::{Fill, GeometryBuilder, Stroke},
-    render::ShapeMaterial,
-    shapes,
-};
 use bevy_twin_stick::{bevy_rapier2d::prelude::Ccd, projectile::ProjectileBundle};
+
+use crate::graphics::Circle;
 
 pub fn basic_bullet() -> ComponentTree {
     return CT!(
         ProjectileBundle::default(),
-        GeometryBuilder::build_as(&shapes::Circle {
-            radius: 5.,
-            center: Vec2::ZERO,
-        }),
-        Fill::color(Color::YELLOW),
-        Stroke::new(Color::BLACK, 2.0),
         Ccd::enabled(),
+        Circle::new(5., Color::BLACK),
         Mesh2dHandle::default(),
-        Handle::<ShapeMaterial>::default(),
         Name::new("Bullet")
     );
 }
