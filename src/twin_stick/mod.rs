b49e4cdbf54513b91ecfca99b0563bcad2a6180d
Zellenon <zellenon@gmail.com>
2025-04-14 04:04:26 -0500

    removed gravity and added projectile components back in

--- src/twin_stick/mod.rs
+++ src/twin_stick/mod.rs
@@ -1,7 +1,7 @@
 use actors::actor_plugin;
 use ai::AIPlugin;
-use avian2d::PhysicsPlugins;
-use bevy::prelude::{App, Plugin};
+use avian2d::{prelude::Gravity, PhysicsPlugins};
+use bevy::{math::Vec2, prelude::{App, Plugin}};
 
 use bevy_turborand::prelude::RngPlugin;
 use camera::CameraPlugin;
@@ -12,12 +12,14 @@ pub mod ai;
 pub mod camera;
 pub mod player;
 pub mod utils;
+pub mod projectile;
 
 pub struct TwinStickPlugin;
 
 impl Plugin for TwinStickPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((RngPlugin::default(), PhysicsPlugins::default()));
+        app.insert_resource(Gravity(Vec2::ZERO));
 
         actor_plugin(app);
         player_plugin(app);
