df533c830bd6ba960ac2f5e21f0ae3f903b2ef00
Zellenon <zellenon@gmail.com>
2025-04-14 04:03:48 -0500

    improved controls handling with fire1,2,3,4 enums

--- src/twin_stick/ai/keyboard.rs
+++ src/twin_stick/ai/keyboard.rs
@@ -2,18 +2,21 @@ use bevy::input::mouse::MouseButton;
 use bevy::prelude::Reflect;
 use bevy::prelude::{Component, KeyCode, Query, With};
 use leafwing_input_manager::{prelude::{ActionState, InputMap, VirtualDPad}, Actionlike, InputControlKind, InputManagerBundle};
+use strum_macros::EnumIter;
 
 use super::super::actors::Actor;
 
 #[derive(Component, Clone, Copy, PartialEq, Eq, Reflect, Debug)]
 pub struct KeyboardAI;
 
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq, Hash, Reflect, Default)]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq, Hash, Reflect, Default, EnumIter)]
 pub(crate) enum PlayerAction {
     #[default]
     Walk,
-    Sprint,
-    Aim
+    Shoot1,
+    Shoot2,
+    Shoot3,
+    Shoot4,
 }
 
 impl Actionlike for PlayerAction {
@@ -28,9 +31,11 @@ impl Actionlike for PlayerAction {
 pub(crate) fn create_player_action_input_manager_bundle() -> InputManagerBundle<PlayerAction> {
     InputManagerBundle::with_map(
         InputMap::new([
-            (PlayerAction::Sprint, KeyCode::ShiftLeft),
+            (PlayerAction::Shoot3, KeyCode::ShiftLeft),
+            (PlayerAction::Shoot4, KeyCode::KeyF),
         ])
-            .with(PlayerAction::Aim, MouseButton::Right)
+            .with(PlayerAction::Shoot1, MouseButton::Left)
+            .with(PlayerAction::Shoot2, MouseButton::Right)
             .with_dual_axis(
                 PlayerAction::Walk,
                 VirtualDPad::new(KeyCode::KeyW, KeyCode::KeyS, KeyCode::KeyA, KeyCode::KeyD)
